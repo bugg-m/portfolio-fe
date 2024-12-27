2f9a016b5a86f663434ea39a67288dc9be3e950e
bugg-m <mkchauhan0543@gmail.com>
2024-12-28 02:37:40 +0530

    path fix

--- eslint.config.js
+++ eslint.config.js
@@ -29,7 +29,7 @@ module.exports = [
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
       'import/no-unresolved': [
-        'off',
+        'error',
         {
           caseSensitive: true,
         },
