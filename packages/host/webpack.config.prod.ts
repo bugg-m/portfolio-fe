b6bc20710d97fbab1783ba74d12869425d0c336b
bugg-m <mkchauhan0543@gmail.com>
2025-01-03 01:35:49 +0530

    fixing build failure

--- packages/host/webpack.config.prod.ts
+++ packages/host/webpack.config.prod.ts
@@ -1,48 +1,16 @@
 import { composePlugins, withNx } from '@nx/webpack';
 import { withReact } from '@nx/react';
 import { withModuleFederation } from '@nx/react/module-federation';
-// import { ModuleFederationConfig } from '@nx/webpack';
+import { ModuleFederationConfig } from '@nx/webpack';
 
 import baseConfig from './module-federation.config';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const prodConfig: any = {
+const prodConfig: ModuleFederationConfig = {
   ...baseConfig,
-  /*
-   * Remote overrides for production.
-   * Each entry is a pair of a unique name and the URL where it is deployed.
-   *
-   * e.g.
-   * remotes: [
-   *   ['app1', 'http://app1.example.com'],
-   *   ['app2', 'http://app2.example.com'],
-   * ]
-   *
-   * You can also use a full path to the remoteEntry.js file if desired.
-   *
-   * remotes: [
-   *   ['app1', 'http://example.com/path/to/app1/remoteEntry.js'],
-   *   ['app2', 'http://example.com/path/to/app2/remoteEntry.js'],
-   * ]
-   */
+
   remotes: [['react_mfe', 'https://bugg-m-react.netlify.app/remoteEntry.js']],
-  shared: {
-    'react-icons': {
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: '^5.4.0',
-    },
-    react: { singleton: true },
-    'react-dom': { singleton: true },
-  },
 };
 
-// Nx plugins for webpack to build config object from Nx options and context.
-/**
- * DTS Plugin is disabled in Nx Workspaces as Nx already provides Typing support for Module Federation
- * The DTS Plugin can be enabled by setting dts: true
- * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
- */
 export default composePlugins(
   withNx(),
   withReact(),
--- packages/host/webpack.config.ts
+++ packages/host/webpack.config.ts
@@ -7,12 +7,6 @@ const config: ModuleFederationConfig = {
   ...baseConfig,
 };
 
-// Nx plugins for webpack to build config object from Nx options and context.
-/**
- * DTS Plugin is disabled in Nx Workspaces as Nx already provides Typing support for Module Federation
- * The DTS Plugin can be enabled by setting dts: true
- * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
- */
 export default composePlugins(
   withNx(),
   withReact(),
