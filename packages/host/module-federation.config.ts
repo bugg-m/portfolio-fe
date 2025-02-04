4fbff7f0b1357d5f26277b209021eb2b8ce9e775
bugg-m <mkchauhan0543@gmail.com>
2025-02-04 19:06:49 +0530

    build fixes

--- packages/host/module-federation.config.ts
+++ packages/host/module-federation.config.ts
@@ -3,7 +3,7 @@ import { ModuleFederationConfig } from '@nx/webpack';
 const config: ModuleFederationConfig = {
   name: 'host',
 
-  remotes: ['react_mfe', 'https://bugg-m-react.netlify.app/remoteEntry.js'],
+  remotes: ['react_mfe', 'https://bugg-m-react.netlify.app'],
   // remotes: [['react_mfe', 'https://localhost:9001/remoteEntry.js']],
 };
 
