99fafade3c231d0afc551c5f93288eae37f16998
bugg-m <mkchauhan0543@gmail.com>
2025-02-05 01:31:59 +0530

    minor change

--- packages/host/module-federation.config.ts
+++ packages/host/module-federation.config.ts
@@ -1,9 +1,12 @@
 import { ModuleFederationConfig } from '@nx/webpack';
 
+const react_mfe_url = process.env.NX_REACT_MFE_URL ?? '';
+
 const config: ModuleFederationConfig = {
   name: 'host',
 
-  remotes: ['react_mfe'],
+  // remotes: ['react_mfe'],
+  remotes: [['react_mfe', react_mfe_url]],
 };
 
 export default config;
