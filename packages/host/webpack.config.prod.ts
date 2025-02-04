e9dadaf48d6abaef420fab8bcdec0f69e5bd633e
bugg-m <mkchauhan0543@gmail.com>
2025-02-05 01:36:34 +0530

    minor build fix

--- packages/host/webpack.config.prod.ts
+++ packages/host/webpack.config.prod.ts
@@ -5,12 +5,12 @@ import { ModuleFederationConfig } from '@nx/webpack';
 
 import baseConfig from './module-federation.config';
 
-const react_mfe_url = process.env.NX_REACT_MFE_URL ?? '';
+// const react_mfe_url = process.env.NX_REACT_MFE_URL ?? '';
 
 const prodConfig: ModuleFederationConfig = {
   ...baseConfig,
 
-  remotes: [['react_mfe', react_mfe_url]],
+  // remotes: [['react_mfe', react_mfe_url]],
 };
 
 export default composePlugins(
