0ba8b2d99816756ac68cd91bd480ded813ca1b9a
bugg-m <mkchauhan0543@gmail.com>
2025-03-10 17:30:48 +0530

    minor build fixes

--- packages/host/src/api/routes/portfolio-api-routes.ts
+++ packages/host/src/api/routes/portfolio-api-routes.ts
@@ -1,8 +1,6 @@
-const baseURL = process.env.NX_PUBLIC_PORTFOLIO_API_URL;
-
 export const PortfolioRoutes = {
-  SEND_MESSAGE: `${baseURL}/sendMessage`,
-  GITHUB_PROJECTS: `${baseURL}/githubProjects`,
-  UPLOAD_CV: `${baseURL}/upload-cv`,
-  DOWNLOAD_CV: `${baseURL}/download-cv`,
+  SEND_MESSAGE: `${process.env.NX_PUBLIC_PORTFOLIO_API_URL}/sendMessage`,
+  GITHUB_PROJECTS: `${process.env.NX_PUBLIC_PORTFOLIO_API_URL}/githubProjects`,
+  UPLOAD_CV: `${process.env.NX_PUBLIC_PORTFOLIO_API_URL}/upload-cv`,
+  DOWNLOAD_CV: `${process.env.NX_PUBLIC_PORTFOLIO_API_URL}/download-cv`,
 };
